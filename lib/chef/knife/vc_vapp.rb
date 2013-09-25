104478a2784b2eb57c6bd4cf4749273fa9cf2075
Stefano Tortarolo <stefano.tortarolo@gmail.com>
2013-09-25 17:40:35 +0200

    Move vc_vapp_* and vc_vm_* in subdirs

---
+++ lib/chef/knife/vc_vapp.rb
@@ -0,0 +1,27 @@
+#
+# Author:: Stefano Tortarolo (<stefano.tortarolo@gmail.com>)
+# Copyright:: Copyright (c) 2012-2013
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'chef/knife/vapp/vc_vapp_config_network'
+require 'chef/knife/vapp/vc_vapp_create'
+require 'chef/knife/vapp/vc_vapp_delete'
+require 'chef/knife/vapp/vc_vapp_reboot'
+require 'chef/knife/vapp/vc_vapp_reset'
+require 'chef/knife/vapp/vc_vapp_show'
+require 'chef/knife/vapp/vc_vapp_start'
+require 'chef/knife/vapp/vc_vapp_stop'
+require 'chef/knife/vapp/vc_vapp_suspend'
