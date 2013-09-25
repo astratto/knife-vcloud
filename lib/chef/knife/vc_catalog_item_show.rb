2451d3579f03c3723eda0db357a06491f89ba681
Stefano Tortarolo <stefano.tortarolo@gmail.com>
2013-09-25 17:43:15 +0200

    Update copyright notice

--- lib/chef/knife/vc_catalog_item_show.rb
+++ lib/chef/knife/vc_catalog_item_show.rb
@@ -1,6 +1,6 @@
 #
 # Author:: Stefano Tortarolo (<stefano.tortarolo@gmail.com>)
-# Copyright:: Copyright (c) 2012
+# Copyright:: Copyright (c) 2012-2013
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
