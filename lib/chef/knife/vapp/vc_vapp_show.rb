27edba2ce310f7a644e122979e91f8b0c671e1bf
Stefano Tortarolo <stefano.tortarolo@gmail.com>
2013-10-23 09:41:19 +0200

    Remove debug statements

--- lib/chef/knife/vapp/vc_vapp_show.rb
+++ lib/chef/knife/vapp/vc_vapp_show.rb
@@ -38,15 +38,6 @@ class Chef
         out_msg("Status", vapp[:status])
         out_msg("IP", vapp[:ip])
 
-        ui.msg("Networks")
-
-        list = [
-            ui.color('Name', :bold),
-            ui.color('IP', :bold)
-        ]
-
-        puts vapp[:networks]
-
         list = [
             ui.color('Name', :bold),
             ui.color('Status', :bold),
