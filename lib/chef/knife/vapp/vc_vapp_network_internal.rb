6edbae6cd2c4998608ca0e810f962752f293a09a
Stefano Tortarolo <stefano.tortarolo@gmail.com>
2013-10-24 12:55:27 +0200

    Add command to add vApp internal networks

--- lib/chef/knife/vapp/vc_vapp_network_internal.rb
+++ lib/chef/knife/vapp/vc_vapp_network_internal.rb
@@ -18,12 +18,47 @@
 
 class Chef
   class Knife
-    class VcVappNetworkExternal < Chef::Knife
+    class VcVappNetworkInternal < Chef::Knife
       include Knife::VcCommon
       include Knife::VcVappCommon
       include Knife::VcNetworkCommon
 
-      banner "knife vc vapp network external [add|delete|edit| [VAPP] [NETWORK] (options)"
+      banner "knife vc vapp network internal [add|delete|edit| [VAPP] [NETWORK] (options)"
+
+      option :gateway,
+             :long => "--gateway GATEWAY",
+             :description => "Set a gateway"
+
+      option :netmask,
+             :long => "--netmask NETMASK",
+             :description => "Set a netmask"
+
+      option :dns1,
+             :long => "--dns1 DNS",
+             :description => "Set a DNS"
+
+      option :dns2,
+             :long => "--dns2 DNS",
+             :description => "Set a DNS"
+
+      option :dns_suffix,
+             :long => "--dns-suffix DNS_SUFFIX",
+             :description => "Set a DNS Suffix"
+
+      option :start_address,
+             :long => "--start-address ADDRESS",
+             :description => "Set a start address"
+
+      option :end_address,
+             :long => "--end-address ADDRESS",
+             :description => "Set a end address"
+
+      option :is_inherited,
+             :long => "--[no-]inherited",
+             :description => "Toggle IsInherited (default false)",
+             :proc => Proc.new { |key| Chef::Config[:knife][:is_inherited] = key },
+             :boolean => true,
+             :default => false
 
       option :retain_network,
              :long => "--[no-]retain-network",
@@ -35,7 +70,7 @@ class Chef
       option :parent_network,
              :short => "-p PARENT_NETWORK",
              :long => "--parent-network PARENT_NETWORK",
-             :description => "Set a parent network. Defaults to the current network.",
+             :description => "Set a parent network for this internal network",
              :proc => Proc.new { |key| Chef::Config[:knife][:parent_network] = key }
 
       def run
@@ -51,38 +86,53 @@ class Chef
 
         command = command_arg.to_sym
 
-        config = {
-          :fence_mode => 'bridged',
-          :retain_network => locate_config_value(:retain_network)
-        }
+        config = { :fence_mode => 'isolated' }
 
         connection.login
 
         vapp = get_vapp(vapp_arg)
-        network = get_network network_arg
+
+        network = vapp[:networks].select{|n| n[:name] == network_arg }.first
 
         unless network
-          raise new ArgumentError, "Network #{network_arg} not found in vDC, please use `vapp network internal` if you want an internal network."
+          if command != :add
+            raise ArgumentError, "Network #{network_arg} not found in vApp, " \
+                                      "please use `--add` if you want to add new network."
+          else
+            network = {:name => network_arg}
+          end
         end
 
         unless command == :delete
+          fields = [:gateway, :netmask, :dns1, :dns2, :dns_suffix,
+                    :start_address, :end_address, :is_inherited, :retain_network]
+
+          fields.each do |field|
+            config[field] = locate_config_value(field)
+
+            if command == :add && config[field].nil?
+              config[field] = ui.ask_question("  #{pretty_symbol(field)}: ")
+            end
+          end
+
           parent_network_arg = locate_config_value(:parent_network)
           if parent_network_arg
             ui.msg "Retrieving parent network details"
             parent_network = get_network parent_network_arg
             config[:parent_network] =  { :id => parent_network[:id],
-                                         :name => parent_network[:name] }
-          else
-            ui.msg "Forcing parent network to itself"
-            config[:parent_network] = { :id => network[:id],
-                                        :name => network[:name] }
+                                        :name => parent_network[:name] }
+          end
+
+          if parent_network && config[:fence_mode] != 'natRouted'
+            ui.info "Setting a parent network for an internal network requires fence mode natRouted. Fixing it..."
+            config[:fence_mode] = 'natRouted'
           end
         end
 
         case command
           when :add
             ui.msg "Adding #{network[:name]} to vApp..."
-            task_id, response = connection.add_org_network_to_vapp vapp[:id], network, config
+            task_id, response = connection.add_internal_network_to_vapp vapp[:id], network, config
             wait_task(connection, task_id)
           when :delete
             ui.msg "Removing #{network[:name]} from vApp..."
